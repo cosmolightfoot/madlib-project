88731dc17474551293b4518d1c40a39016f8baff
Cosmo Kent <cosmolightfoot@gmail.com>
2019-01-17 23:39:57 -0800

    created variables for inputs, with getElementById

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 function madLib() {
     //link each text input 
-    var adjective = document.getElementById('adjective').value;
-    var place = document.getElementById('place').value;
+    var adjective1 = document.getElementById('adjective-1').value;
+    var adjective2 = document.getElementById('adjective-2').value;
+    var pluralNoun1 = document.getElementById('plural-noun-1').value;
+    var pluralNoun2 = document.getElementById('plural-noun-2').value;
+    var adjective3 = document.getElementById('adjective-3').value;
     var noun1 = document.getElementById('noun-1').value;
     var noun2 = document.getElementById('noun-2').value;
-    var verb = document.getElementById('verb').value;
-    var actionVerb = document.getElementById('action-verb').value;
-    console.log(adjective, place, noun1, noun2, verb, actionVerb)
+    var pluralNoun3 = document.getElementById('plural-noun-3').value;
+    var adjective4 = document.getElementById('adjective-4').value;
+    var personInRoom = document.getElementById('person-in-room').value;
+    var adjective5 = document.getElementById('adjective-5').value;
+    var noun3 = document.getElementById('noun-3').value;
+    var adjective6 = document.getElementById('adjective-6').value;
+    var pluralNoun4 = document.getElementById('adjective-4').value;
+    var pluralNoun5 = document.getElementById('adjective-5').value;
+    var adjective7 = document.getElementById('adjective-7').value;
+    var adjective8 = document.getElementById('adjective-8').value;
+    var adjective9 = document.getElementById('adjective-9').value;
+    console.log(adjective1, adjective2, pluralNoun1, pluralNoun2, adjective3, noun1, noun2, pluralNoun3, adjective4, personInRoom, adjective5, noun3, adjective6, pluralNoun4, pluralNoun5, adjective7, adjective8, adjective9)
     //display the value 
     
 }
