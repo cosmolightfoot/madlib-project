bf061465fe162ec3870ea1dd2680924f08b216be
Cosmo Kent <cosmolightfoot@gmail.com>
2019-01-17 21:05:14 -0800

    created mad lib, began js

---
+++ main.js
@@ -0,0 +1,12 @@
+function madLib() {
+    //link each text input 
+    var adjective = document.getElementById('adjective').value;
+    var place = document.getElementById('place').value;
+    var noun1 = document.getElementById('noun-1').value;
+    var noun2 = document.getElementById('noun-2').value;
+    var verb = document.getElementById('verb').value;
+    var actionVerb = document.getElementById('action-verb').value;
+    //retrieve the value from each text input
+    
+    //display the value 
+}
