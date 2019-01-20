1f06106888db86c860a3e0eb9964288c8ac93b71
Cosmo Kent <cosmolightfoot@gmail.com>
2019-01-20 13:42:57 -0800

    randomize button works in test phase

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # madlib-project
+Feature Ideas:
+- Create condition where website will not display madlib until text is inputted in every box
+- Create randomize button that draws words from an array autofill the madlib
+- make filled in words bold 
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function madLib() {
-    //link each text input 
+    //links string input to js variables
     var adjective1 = document.getElementById('adjective-1').value;
     var adjective2 = document.getElementById('adjective-2').value;
     var pluralNoun1 = document.getElementById('plural-noun-1').value;
@@ -20,9 +20,31 @@ function madLib() {
     var adjective8 = document.getElementById('adjective-8').value;
     var adjective9 = document.getElementById('adjective-9').value;
     console.log(adjective1, adjective2, pluralNoun1, pluralNoun2, adjective3, noun1, noun2, pluralNoun3, adjective4, personInRoom, adjective5, noun3, adjective6, noun4, pluralNoun4, pluralNoun5, adjective7, adjective8, adjective9);
-    //display the value 
     
+    //creates madlib body once input fields are recognized
     var madLibBody = 'Ladies and gentlemen, on this ' + adjective1 + ' occasion it is a privelage to address such a/an ' + adjective2 + ' looking group of ' + pluralNoun1 + '. I can tell from your smiling ' + pluralNoun2 + ' that you will support my ' + adjective3 + ' program in the coming election. I promise that, if elected, there will be a/an ' + noun1 + ' in every ' + noun2 + ' and two ' + pluralNoun3 + ' in every garage. I want to warn you about my ' + adjective4 + ' opponent, Mr. ' + personInRoom + '. The man is nothing but a/an ' + adjective5 + ' ' + noun3 + '. He has a ' + adjective6 + ' character and is working ' + noun4 + ' in glove with the criminal element. If elected I promise to eliminate vice. I will keep the ' + pluralNoun4 + ' off the city streets. I will keep the crooks from dipping their ' + pluralNoun5 + ' in the public till. I promise you ' + adjective7 + ' government, ' + adjective8 + ' taxes, and ' + adjective9 + ' schools.';
     var displayMadLib = document.getElementById('madlib-body');
     displayMadLib.textContent = madLibBody;
+}
+
+function randomizer() {
+    document.getElementById('adjective-1').value = "testing 123"
+    document.getElementById('adjective-2').value = "testing 123"
+    document.getElementById('plural-noun-1').value = "testing 123"
+    document.getElementById('plural-noun-2').value = "testing 123"
+    document.getElementById('adjective-3').value = "testing 123"
+    document.getElementById('noun-1').value = "testing 123"
+    document.getElementById('noun-2').value = "testing 123"
+    document.getElementById('plural-noun-3').value = "testing 123"
+    document.getElementById('adjective-4').value = "testing 123"
+    document.getElementById('person-in-room').value = "testing 123"
+    document.getElementById('adjective-5').value = "testing 123"
+    document.getElementById('noun-3').value = "testing 123"
+    document.getElementById('adjective-6').value = "testing 123"
+    document.getElementById('noun-4').value = "testing 123"
+    document.getElementById('plural-noun-4').value = "testing 123"
+    document.getElementById('plural-noun-5').value = "testing 123"
+    document.getElementById('adjective-7').value = "testing 123"
+    document.getElementById('adjective-8').value = "testing 123"
+    document.getElementById('adjective-9').value = "testing 123"
 }
