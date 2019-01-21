ba66e88b5b5962640b70b3db8f88a4d737b792f7
Cosmo Kent <cosmolightfoot@gmail.com>
2019-01-20 16:28:26 -0800

    all arrays compiled; randomizer works :)

--- main.js
+++ main.js
@@ -3,23 +3,23 @@ function madLib() {
     //links string input to js variables
     var adjective1 = document.getElementById('adjective-1').value;
     var adjective2 = document.getElementById('adjective-2').value;
-    var pluralNoun1 = document.getElementById('plural-noun-1').value;
-    var pluralNoun2 = document.getElementById('plural-noun-2').value;
     var adjective3 = document.getElementById('adjective-3').value;
-    var noun1 = document.getElementById('noun-1').value;
-    var noun2 = document.getElementById('noun-2').value;
-    var pluralNoun3 = document.getElementById('plural-noun-3').value;
     var adjective4 = document.getElementById('adjective-4').value;
-    var personInRoom = document.getElementById('person-in-room').value;
     var adjective5 = document.getElementById('adjective-5').value;
-    var noun3 = document.getElementById('noun-3').value;
     var adjective6 = document.getElementById('adjective-6').value;
-    var noun4 = document.getElementById('noun-4').value;
-    var pluralNoun4 = document.getElementById('adjective-4').value;
-    var pluralNoun5 = document.getElementById('adjective-5').value;
     var adjective7 = document.getElementById('adjective-7').value;
     var adjective8 = document.getElementById('adjective-8').value;
     var adjective9 = document.getElementById('adjective-9').value;
+    var pluralNoun1 = document.getElementById('plural-noun-1').value;
+    var pluralNoun2 = document.getElementById('plural-noun-2').value;
+    var pluralNoun3 = document.getElementById('plural-noun-3').value;
+    var pluralNoun4 = document.getElementById('plural-noun-4').value;
+    var pluralNoun5 = document.getElementById('plural-noun-5').value;
+    var noun1 = document.getElementById('noun-1').value;
+    var noun2 = document.getElementById('noun-2').value;
+    var noun3 = document.getElementById('noun-3').value;
+    var noun4 = document.getElementById('noun-4').value;
+    var personInRoom = document.getElementById('person-in-room').value;
     console.log(adjective1, adjective2, pluralNoun1, pluralNoun2, adjective3, noun1, noun2, pluralNoun3, adjective4, personInRoom, adjective5, noun3, adjective6, noun4, pluralNoun4, pluralNoun5, adjective7, adjective8, adjective9);
     
     //creates madlib body once input fields are recognized
@@ -47,7 +47,7 @@ function randomizer() {
     var nounSelector2 = Math.floor(Math.random() * allNouns.length);
     var nounSelector3 = Math.floor(Math.random() * allNouns.length);
     var nounSelector4 = Math.floor(Math.random() * allNouns.length);
-    var personInRoomSelector = Math.floor(Math.random() * 100);
+    var personInRoomSelector = Math.floor(Math.random() * allNames.length);
     
 
     document.getElementById('adjective-1').value = allAdjectives[adjectiveSelector1];
@@ -68,7 +68,7 @@ function randomizer() {
     document.getElementById('noun-2').value = allNouns[nounSelector2];
     document.getElementById('noun-3').value = allNouns[nounSelector3];
     document.getElementById('noun-4').value = allNouns[nounSelector4];
-    document.getElementById('person-in-room').value = 'testing 123';
+    document.getElementById('person-in-room').value = allNames[personInRoomSelector];
 }
 console.log(allAdjectives[3]);
 console.log(allAdjectives.length);
